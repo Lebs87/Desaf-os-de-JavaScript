e6b58b9d02574f05d4a1b989ca296b6e79f8c06f
lebs87 <lebs87@gmail.com>
2022-08-29 14:22:36 -0300

    Primer ejercicio de JavaScript con algunas pruebas mas

---
+++ js/index.js
@@ -0,0 +1,38 @@
+//Repeticiones de un texto
+
+let repeticiones = parseInt(prompt("Ingrese número de repeticiones deseadas"));
+let texto = prompt ("Ingresar texto a repetir");
+
+debugger
+
+for (let i=0; i<repeticiones; i++){
+    console.log(texto);
+}
+
+//División de números
+
+/*let numero1 = parseInt(prompt("Ingrese número a dividir"))
+let numero2 = parseInt(prompt("Ingrese divisor"))
+
+debugger
+
+let resultado = numero1 / numero2;
+alert(resultado);*/
+
+let numero3 = varios3
+let numero4 = varios4
+
+let numero5 = varios5
+let numero6 = varios6
+
+let numero7 = varios7
+let numero8 = varios8
+
+let numero9 = varios9
+let varios10 = varios10
+
+//Ejercicios con for 
+
+for (let a=0; a<repeticiones; a++){
+    console.log(texto);
+}
